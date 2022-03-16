e9766897e44dda18dddea913f3585bb3ccccb34b
ezygarlowska <emilia.zygarlowska@gmail.com>
2022-03-16 12:55:11 +0100

    .

--- MWC_Project_7.m
+++ MWC_Project_7.m
@@ -4,7 +4,7 @@ clear; clc; close all;
 
 %% Load the data 
 
-%Loading the files LowTide.txt, midTide.txt and highTide.txt
+%Loading the files for each condition
 ucData = load('ucData.mat');
 condition1=ucData.condition1;
 condition2=ucData.condition2;
@@ -35,6 +35,7 @@ h3 = condition3.h;
 % Number of blocks is equal 15, so that we have 30 degrees of freedom
 x= [eta1 eta2 eta3];
 Fs=condition1.Fs; %it is the same for each of the conditions
+
 figure;
 for i=1:3
     subplot(3,1,i);
@@ -45,10 +46,13 @@ for i=1:3
     if i==1 
         title('Variance density spectra of the free-surface elevation');
     end 
+    if i==3 
+        %xlabel('Frequency [Hz]','FontWeight','bold');
+    end 
     %ylabel('E [m^2/Hz]','FontWeight','bold');
     legend('Spectrum (N=15)');
 end
-%xlabel('Frequency [Hz]','FontWeight','bold');
+
 
 %add labels and confidence intervals, remember that we did something wrong
 %in second practical, so go to the feedback and fix conf95 thingy
@@ -59,7 +63,7 @@ end
 
 %sea-swell 0.05 Hz < f < fN
 fmin=0.05;
-fmax=1/(2*0.5);
+fmax=1/(2*0.5); %Nyquist frequency
     for i=1:3
         [S f edf conf95Interval] = VarianceDensitySpectrum(x(:,i),0.5^3*length(x(:,i)),Fs);
         
@@ -95,15 +99,13 @@ end
 display(rwh);
 display(ratio);
 
-%we should maybe make a nice plot with all the characteristics or smth
-
 %% skewness and asymmetry
 
 %Compute the skewness and asymmetry of the sea-swell waves for each case.
 
 % filtering the free-surface time-series to remove the low- frequency variations 
 flow=0.05;
-fhigh=1/(2*0.5);
+fhigh=1/(2*0.5); 
 for i=1:3
     dataFilt (:,i)= fft_filter(x(:,i), Fs,flow , fhigh);
 end 
@@ -140,7 +142,7 @@ end
 u=[u1 u2 u3];
 
 for i=1:3
-    u_mean(i)=mean(u(i));
+    u_mean(i)=mean(u(:,i));
 end 
 
 display(u_mean);
@@ -161,11 +163,12 @@ for i=1:3
 end 
 
 % checking the computations
-sum=u_lf(:,1)+u_hf(:,1)+u_mean(1);
-diff=u1-sum;
-mean_diff=mean(diff);
-display(mean_diff);
-%!!!!!! idk if thi part is correct, because the difference seems to be quite high!!!!!!!
+sum1=u_lf(:,1)+u_hf(:,1)+u_mean(1);
+figure;
+scatter(u1, sum1);
+title('Correlation between computed and observed values','FontWeight','bold');
+ylabel('u_l_f+u_h_f+u_m_e_a_n [m/s]','FontWeight','bold');
+xlabel('non-filtered velocity u [m/s]','FontWeight','bold');
 %% plot now uhf for each of the three cases
 
 figure;
@@ -173,7 +176,7 @@ for i=1:3
     subplot(3,1,i);
     plot(time,u_hf(:,i));
     hold on;
-    plot(time,u_lf(:,i));
+    plot(time,u_lf(:,i),'LineWidth',2);
     grid on;
     if i==1 
         title('Cross shore velocity','FontWeight','bold');
@@ -221,26 +224,23 @@ for i=1:3
     subplot(2,1,1);
     plot(time,u_diff(:,i));
     hold on;
-    plot(time,u_lf(:,i));
+    plot(time,u_lf(:,i),'LineWidth',2);
     grid on;
     title('velocity time-series- ',condition(i),'FontWeight','bold');
     xlabel('time [s]','FontWeight','bold');
     legend(["u" "u_l_f"]);
     ylabel('cross shore velocity [m/s]','FontWeight','bold');
-    xlim([0 2048]);
+    xlim([500 800]);
     
     %concentration 
     subplot(2,1,2);
-    plot(time,c_diff(:,i));
-    hold on;
-    plot(time,c_lf(:,i));
-    plot(time,c_hf(:,i));
+    plot(time,c(:,i));
     grid on;
     title('concentration time-series','FontWeight','bold');
     xlabel('time [s]','FontWeight','bold');
-    legend(["c" "c_h_f" "c_l_f"]);
+    legend(["c"]);
     ylabel('concentration [m/s]','FontWeight','bold'); % add unit
-    xlim([0 2048]);
+    xlim([500 800]);
 end
 
 %% 8.2.2. Sediment transport calculations
@@ -248,17 +248,21 @@ end
 %% Calculate the total sediment transport q; Calculate the three parts of the sediment transport for each condition
 
 for i=1:3
-    q(i)=mean(c(i).*u(i)); %total sediment transport
+    q(i)=mean(c(:,i).*u(:,i)); %total sediment transport
     uc_hf(i)=mean(u_hf(:,i).*c_hf(:,i)); %high frequency component
     uc_lf(i)=mean(u_lf(:,i).*c_lf(:,i)); %low frequency component
-    uc(i)=mean(u(i)).*mean(c(i)); %product of means
+    uc(i)=mean(u(:,i)).*mean(c(:,i)); %product of means
 end 
 
 % checking whether the result is valid (for the first condition)
-sum= uc(1)+uc_hf(1)+uc_lf(1);
-diff=q(1)-sum;
+for i=1:3
+    sum(i)= uc(i)+uc_hf(i)+uc_lf(i);
+end 
+diff=q-sum;
+ratio=100*q./sum;
 display(diff);
+display(ratio);
 
-%we should ask the TA what difference is okay to have
+
 
 
